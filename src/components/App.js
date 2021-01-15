76e971f0c94a1d4a599fe0bae46d655d724f7714
acdale5774 <acdale5774@gmail.com>
2021-01-14 21:57:55 -0500

    front-end

--- src/components/App.js
+++ src/components/App.js
@@ -72,6 +72,22 @@ class App extends Component {
         }
     }
 
+    stakeTokens = (amount) => {
+        this.setState({ loading: true })
+        this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+            this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+                this.setState({ loading: false })
+      })
+    })
+    }
+
+    unstakeTokens = (amount) => {
+        this.setState({ loading: true })
+        this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
+            this.setState({ loading: false })
+        })
+    }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -91,7 +107,13 @@ class App extends Component {
     if(this.state.loading) {
       content = <p id="loader" className="text-center">Loading...</p>
     } else {
-        content = <Main />
+        content = <Main
+            daiTokenBalance={this.state.daiTokenBalance}
+            dappTokenBalance={this.state.dappTokenBalance}
+            stakingBalance={this.state.stakingBalance}
+            stakeTokens={this.stakeTokens}
+            unstakeTokens={this.unstakeTokens}
+        />
     }
 
     return (
